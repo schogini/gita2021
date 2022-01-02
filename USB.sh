acf9d2075ea90500646565a686596f63b1c8cd82
Sreeprakash Neelkantan <sree@schogini.com>
2022-01-02 16:20:25 +0530

    sree

---
+++ USB.sh
@@ -0,0 +1,92 @@
+
+[ $(ls -l /Volumes/| grep -c SAREGAMA-S) -ne 1 ] && echo Insert USB && exit
+
+
+find /Volumes/SAREGAMA-S -name "\.*" -exec rm -fr {} \; 2> /dev/null
+
+cd /Volumes/SAREGAMA-S
+
+list=""
+
+show(){
+ #echo $list
+ #return
+ cd /Volumes/SAREGAMA-S
+ i=1
+ #for f in $(ls -lt `find . -name "*.mp3" `|awk '{print $9}'|tac)
+ for f in $list
+ do
+  ii=$(printf "%02d\n" $i) 
+  echo $ii $f
+  let i=$i+1
+ done
+}
+
+if [ $# -ne 0 ]; then
+ show
+ exit
+fi
+
+t(){
+ list="$list $1"
+ qq=$(printf "%02d\n" $q) 
+ #echo $1
+ touch -m -t "${qq}01010101" $1
+ let q=$q+1
+ #sleep 1
+}
+
+q=1
+t 1.mp3
+t 2.mp3
+t 3.mp3
+t 4.mp3
+t 5.mp3
+t 6.mp3
+t 7.mp3
+t 8.mp3
+t 9.mp3
+t 10.mp3
+t 11.mp3
+t 12.mp3
+t 13.mp3
+t 14.mp3
+t 15.mp3
+t 16.mp3
+t 17.mp3
+t 18.mp3
+t 991-Morning.mp3
+t HanumanChalisa.mp3
+
+t CH-S-P
+t MISC
+
+cd CH-S-P
+q=1
+t 1.mp3
+t 2.mp3
+t 3.mp3
+t 4.mp3
+t 5.mp3
+t 6.mp3
+t 7.mp3
+t 8.mp3
+t 9.mp3
+t 10.mp3
+t 11.mp3
+t 12.mp3
+t 13.mp3
+t 14.mp3
+t 15.mp3
+t 16.mp3
+t 17.mp3
+t 18.mp3
+
+cd ../MISC
+q=1
+t Saraswati-Mantra-For-Concentration-In-Studies-OM-Shreem-Hreem-Saraswatyai-Namah.mp3
+t study-music.mp3
+t vipassana.mp3
+t ANCIENT-SARASWATI-MANTRA-FOR-A-SHARP-MIND-AND-FOCUS.mp3
+
+show
